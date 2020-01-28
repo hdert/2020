42fa136337f0e2ba1ff9f71301aef8193309754f
hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2020-01-28 17:33:22 +1300

    add for loop and flag instead of while true

--- python/calculatorFlag.py
+++ python/calculatorFlag.py
@@ -1,13 +1,12 @@
 print("This is an addition calculator")
-for i in range(0, 3):
+run = 1
+while run == 1:
     try:
         print("Input your first number, press enter, then input your second number, press enter.")
         conditionOne = float(input())
         conditionTwo = float(input())
         result = conditionOne + conditionTwo
         print("Your result is: {}".format(result))
-        break
+        run = 0
     except ValueError:
         print("Sorry but your input wasn't in the correct format.")
-        if i == 2:
-            print("You're a failure")
