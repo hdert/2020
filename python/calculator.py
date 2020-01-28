8bf6a44c5e695dc58044b5dad225880cea898993
hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2020-01-28 17:11:18 +1300

    make code more readable

--- python/calculator.py
+++ python/calculator.py
@@ -2,7 +2,10 @@ print("This is an addition calculator")
 while True:
     try:
         print("Input your first number, press enter, then input your second number, press enter.")
-        print("Your result is: {}".format(float(input()) + float(input())))
+        conditionOne = float(input())
+        conditionTwo = float(input())
+        result = conditionOne + conditionTwo
+        print("Your result is: {}".format(result))
         break
     except ValueError:
         print("Sorry but your input wasn't in the correct format.")
