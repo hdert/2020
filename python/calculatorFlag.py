0981344f04b89d8c7ec081f694ffb43efe56cfbf
hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2020-01-30 14:30:16 +1300

    fix flags in python

--- python/calculatorFlag.py
+++ python/calculatorFlag.py
@@ -1,12 +1,12 @@
 print("This is an addition calculator")
-run = 1
-while run == 1:
+run = True
+while run == True:
     try:
         print("Input your first number, press enter, then input your second number, press enter.")
         conditionOne = float(input())
         conditionTwo = float(input())
         result = conditionOne + conditionTwo
         print("Your result is: {}".format(result))
-        run = 0
+        run = False
     except ValueError:
         print("Sorry but your input wasn't in the correct format.")
