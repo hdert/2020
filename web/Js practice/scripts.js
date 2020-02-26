7730830db2566c19371157d1ada1fea9130e8a58
hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2020-02-27 09:33:44 +1300

    added web files

---
+++ web/Js practice/scripts.js
@@ -0,0 +1,16 @@
+function change() {
+    var body = document.getElementsByTagName('body')[0];
+    if (body.style.backgroundColor == 'blue') {
+        body.style.backgroundColor = 'white';
+    }else{
+        body.style.backgroundColor = 'blue';
+    }
+}
+
+function colorChange() {
+    document.getElementsByTagName('p')[1].innerHTML='Try this!';
+}
+
+function tryi(id, nameOf) {
+    nameOf.textContent = 'Hello!';
+}
