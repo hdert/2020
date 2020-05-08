eabc6b05c95dc9e4c54bbb2e4f1fb0423f248590
hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2020-05-08 16:28:43 +1200

    fix and beautify

--- web/Js practice/scripts.js
+++ web/Js practice/scripts.js
@@ -2,13 +2,13 @@ function change() {
     var body = document.getElementsByTagName('body')[0];
     if (body.style.backgroundColor == 'blue') {
         body.style.backgroundColor = 'white';
-    }else{
+    } else {
         body.style.backgroundColor = 'blue';
     }
 }
 
 function colorChange() {
-    document.getElementsByTagName('p')[1].innerHTML='Try this!';
+    document.getElementsByTagName('p')[1].innerHTML = 'Try this!';
 }
 
 function tryi(id, nameOf) {
