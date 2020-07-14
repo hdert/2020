934311315308149ed3d1cf3c5bcb18f75e6b9a37
hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2020-07-14 16:37:05 +1200

    create diceware passphrase generator

---
+++ python/diceware.py
@@ -0,0 +1,7 @@
+from random import randint as rand
+times = int(input("How many words would you like? "))
+for x in range(0, times):
+  for y in range(0, 4):
+    print(rand(1,6), end='')
+    if y == 3:
+      print(rand(1,6), end='\n')
