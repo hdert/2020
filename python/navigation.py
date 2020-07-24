9d17a5ec25b3dad960bf5965381f73ed2f8920fc
hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2020-07-24 14:26:30 +1200

    Make navigation.py, modify .gitignore

---
+++ python/navigation.py
@@ -0,0 +1,19 @@
+'''
+Author: Justin Muirhead
+'''
+
+room = 0  # location
+rooms = {'Hall': 'Hall with carpet',
+         'Hall2': 'Hall with paintings',
+         'Courtyard': 'Courtyard with paving stones',
+         'Hall3': 'Hallway with plants',
+         'Kitchen': 'Kitchen has old moldy food',
+         'Hall4': 'Hallway has rat droppings',
+         'Hall5': 'Hallway has dusty cobwebs',
+         'Bedroom': 'Bedroom has a bed in it',
+         'CheeseRoom': 'Has cheese in it'}
+
+n = []
+s = []
+e = []
+w = []
