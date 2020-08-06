099c2b1662a6ffd574ef31c389e90a687409d7e1
hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2020-08-06 12:39:32 +1200

    update block.py

--- python/numericCourse/block.py
+++ python/numericCourse/block.py
@@ -1,23 +1,8 @@
 def main(uChoice):
-    i = 1
-    x = 0
-    while x <= 100:
-        if i % uChoice == 0:
-            x += 1
-        i += 1
-    maxLength = len(str(i)) + 1
-    i = 1
-    x = 0
-    z = 1
-    while x < 100:
-        if i % uChoice == 0:
-            print(f"{i:4}", end='')
-            x += 1
-            z += 1
-        if z % 11 == 0:
-            print()
-            z = 1
-        i += 1
+    for x in range(10):
+        for i in range(1, 11):
+            print(f"{(x * 10 + i) * uChoice:4}", end='')
+        print()
 
 
 if __name__ == "__main__":
