7a98b15bde0ef0b789f8d42b1f0ed7e4c5aff0c8
hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2020-08-14 15:11:34 +1200

    add rowid showDb make linePrint seperate function

--- python/databaseInternal/main.py
+++ python/databaseInternal/main.py
@@ -15,11 +15,22 @@ def connDb():
     return conn, conn.cursor()
 
 
+def linePrint(records, unameLen):
+    idLen = len(str(len(records))) + 1
+    scoreLen = len(str(records[0][2])) + 3
+    spaces = 10
+    print("—" * (spaces + unameLen + scoreLen + idLen))
+
+
 def showDb(c):
-    c.execute("SELECT * FROM players ORDER BY score DESC")
+    c.execute("SELECT rowid,* FROM players ORDER BY score DESC")
     records = c.fetchall()
+    linePrint(records, 7)
+    print(f"|{'id':^4}|{'name':^9}|{'score':^7}|")
     for i in records:
-        print(i[0], f"{i[1]:{len(str(records[0][1]))}}")
+        print('|', f"{i[0]:>2}", '|', i[1], '|',
+              f"{i[2]:{len(str(records[0][2])) + 3}}", '|')
+    linePrint(records, 7)
 
 
 def main():
