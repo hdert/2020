645d5da94a85980a3150863c5f7ba6e8041dec9c
hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2020-08-18 12:34:01 +1200

    redo line length calculation

--- python/databaseInternal/main.py
+++ python/databaseInternal/main.py
@@ -15,22 +15,29 @@ def connDb():
     return conn, conn.cursor()
 
 
-def linePrint(records, unameLen):
-    idLen = len(str(len(records))) + 1
-    scoreLen = len(str(records[0][2])) + 3
+def linePrint(records, lenId, lenUsername, lenScore):
     spaces = 10
-    print("—" * (spaces + unameLen + scoreLen + idLen))
+    print("—" * (spaces + lenId + lenUsername + lenScore))
 
 
 def showDb(c):
     c.execute("SELECT rowid,* FROM players ORDER BY score DESC")
     records = c.fetchall()
-    linePrint(records, 7)
-    print(f"|{'id':^4}|{'name':^9}|{'score':^7}|")
+    lenScore = len(str(records[0][2]))
+    if lenScore < 5:
+        lenScore = 5
+    lenId = len(str(len(records)))
+    if lenId < 2:
+        lenId = 2
+    lenUsername = len(records[0][1])
     for i in records:
-        print('|', f"{i[0]:>2}", '|', i[1], '|',
-              f"{i[2]:{len(str(records[0][2])) + 3}}", '|')
-    linePrint(records, 7)
+        if len(i[1]) > lenUsername:
+            lenUsername = len(i[1])
+    linePrint(records, lenId, lenUsername, lenScore)
+    print(f"| {'id':>{lenId}} | {'name':^{lenUsername}} | {'score':>{lenScore}} |")
+    for i in records:
+        print(f"| {i[0]:{lenId}} | {i[1]:{lenUsername}} | {i[2]:{lenScore}} |")
+    linePrint(records, lenId, lenUsername, lenScore)
 
 
 def main():
