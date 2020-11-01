2e33193db6102e4d81a0216707616154a37e8e73
hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2020-11-01 22:05:43 +1300

    clean up conventions

--- python/databaseInternal/main.py
+++ python/databaseInternal/main.py
@@ -3,7 +3,7 @@ from sqlite3 import Error
 import os.path
 
 
-def connDb():
+def conn_db():
     conn = None
     db_file = "main.db"
 
@@ -12,42 +12,44 @@ def connDb():
     except Error as e:
         print(e)
 
-    return conn, conn.cursor()
+    return conn.cursor()
 
 
-def linePrint(records, lenId, lenUsername, lenScore):
+def line_print(len_id, len_username, len_score):
     spaces = 10
-    print("—" * (spaces + lenId + lenUsername + lenScore))
+    print("—" * (spaces + len_id + len_username + len_score))
 
 
-def showDb(c):
+def show_db(c):
     c.execute("SELECT rowid,* FROM players ORDER BY score DESC")
     records = c.fetchall()
-    lenScore = len(str(records[0][2]))
-    if lenScore < 5:
-        lenScore = 5
-    lenId = len(str(len(records)))
-    if lenId < 2:
-        lenId = 2
-    lenUsername = len(records[0][1])
+    len_score = len(str(records[0][2]))
+    if len_score < 5:
+        len_score = 5
+    len_id = len(str(len(records)))
+    if len_id < 2:
+        len_id = 2
+    len_username = len(records[0][1])
     for i in records:
-        if len(i[1]) > lenUsername:
-            lenUsername = len(i[1])
-    linePrint(records, lenId, lenUsername, lenScore)
+        if len(i[1]) > len_username:
+            len_username = len(i[1])
+    line_print(len_id, len_username, len_score)
     print(
-        f"| {'id':>{lenId}} | {'name':^{lenUsername}} | {'score':>{lenScore}} |"
+        f"| {'id':>{len_id}} | {'name':^{len_username}} | {'score':>{len_score}} |"
     )
     for i in records:
-        print(f"| {i[0]:{lenId}} | {i[1]:{lenUsername}} | {i[2]:{lenScore}} |")
-    linePrint(records, lenId, lenUsername, lenScore)
+        print(
+            f"| {i[0]:{len_id}} | {i[1]:{len_username}} | {i[2]:{len_score}} |"
+        )
+    line_print(len_id, len_username, len_score)
 
 
 def main():
     if not os.path.isfile('main.db'):
         import dbCreate
         dbCreate.main()
-    conn, c = connDb()
-    showDb(c)
+    c = conn_db()
+    show_db(c)
 
 
 if __name__ == "__main__":
