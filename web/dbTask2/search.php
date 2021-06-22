90ba0923129c845c76761cb4541ceb1ea4f50e27
hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2021-06-23 09:18:28 +1200

    fix search, rename images

--- web/dbTask2/search.php
+++ web/dbTask2/search.php
@@ -17,41 +17,41 @@
 
     <form method="POST">
       <label>Search</label>
-      <input type="text" name="search" value="<?php print $_POST['search'] ?>" autofocus onfocus="let x=this.value; this.value=null; this.value=x" />
+      <input type="text" name="search" value="<?php if (isset($_POST['search'])){print $_POST['search'];}?>" autofocus onfocus="let x=this.value; this.value=null; this.value=x" />
       <select name="select" onchange="checkSelect();" id="select">
-        <option value="name" <?php if ($_POST['select'] == "name") {
+        <option value="name" <?php if (isset($_POST['select'])){if ($_POST['select'] == "name") {
                                 print "selected=\"selected\"";
-                              } ?>>Name</option>
-        <option value="cost" <?php if ($_POST['select'] == "cost") {
+                              }}?>>Name</option>
+        <option value="cost" <?php if (isset($_POST['select'])){if ($_POST['select'] == "cost") {
                                 print "selected=\"selected\"";
-                              } ?>>Cost</option>
-        <option value="rating" <?php if ($_POST['select'] == "rating") {
+                              }} ?>>Cost</option>
+        <option value="rating" <?php if (isset($_POST['select'])){if ($_POST['select'] == "rating") {
                                   print "selected=\"selected\"";
-                                } ?>>Rating</option>
+                                }} ?>>Rating</option>
       </select>
       <div id="radio">
         <label for="radio">&#60</label>
-        <input type="radio" name="costSort" value="<" <?php if ($_POST['costSort'] == "<" && $_POST['select'] == "cost") {
+        <input type="radio" name="costSort" value="<" <?php if(isset($_POST['costSort'])){if ($_POST['costSort'] == "<" && $_POST['select'] == "cost") {
                                                         print "checked=\"checked\"";
-                                                      } ?> />
+                                                      }} ?> />
         <label for="radio">&#60&#61</label>
-        <input type="radio" name="costSort" id="defaultCostSort" value="<=" <?php if ($_POST['costSort'] == "<=") {
+        <input type="radio" name="costSort" id="defaultCostSort" value="<=" <?php if(isset($_POST['costSort'])){if ($_POST['costSort'] == "<=") {
                                                                               print "checked=\"checked\"";
                                                                             } else if ($_POST['costSort'] == "" || $_POST['select'] != "cost") {
                                                                               print "checked=\"checked\"";
-                                                                            } ?> />
+                                                                            }} ?> />
         <label for="radio">&#61</label>
-        <input type="radio" name="costSort" value="=" <?php if ($_POST['costSort'] == "=" && $_POST['select'] == "cost") {
+        <input type="radio" name="costSort" value="=" <?php if(isset($_POST['costSort'])){if ($_POST['costSort'] == "=" && $_POST['select'] == "cost") {
                                                         print "checked=\"checked\"";
-                                                      } ?> />
+                                                      }} ?> />
         <label for="radio">&#62&#61</label>
-        <input type="radio" name="costSort" value=">=" <?php if ($_POST['costSort'] == ">=" && $_POST['select'] == "cost") {
+        <input type="radio" name="costSort" value=">=" <?php if(isset($_POST['costSort'])){if ($_POST['costSort'] == ">=" && $_POST['select'] == "cost") {
                                                           print "checked=\"checked\"";
-                                                        } ?> />
+                                                        }} ?> />
         <label for="radio">&#62</label>
-        <input type="radio" name="costSort" value=">" <?php if ($_POST['costSort'] == ">" && $_POST['select'] == "cost") {
+        <input type="radio" name="costSort" value=">" <?php if(isset($_POST['costSort'])){if ($_POST['costSort'] == ">" && $_POST['select'] == "cost") {
                                                         print "checked=\"checked\"";
-                                                      } ?> />
+                                                      }} ?> />
       </div>
       <input type="submit" value="submit" />
     </form>
@@ -65,52 +65,54 @@
 
       <?php
       include("./connect.php");
-      $search = $_POST['search'];
-      $select = $_POST['select'];
+      if (isset($_POST['search']) && isset($_POST['select'])){
+        $search = $_POST['search'];
+        $select = $_POST['select'];
 
-      if ($search) {
-        if ($select == "cost") {
-          switch ($_POST['costSort']) {
-            case "<":
-              $query = $pdo->prepare("SELECT * FROM `games` WHERE `cost` < ?");
-              break;
-            case "<=":
-              $query = $pdo->prepare("SELECT * FROM `games` WHERE `cost` <= ?");
-              break;
-            case "=":
-              $query = $pdo->prepare("SELECT * FROM `games` WHERE `cost` = ?");
-              break;
-            case ">=":
-              $query = $pdo->prepare("SELECT * FROM `games` WHERE `cost` >= ?");
-              break;
-            case ">":
-              $query = $pdo->prepare("SELECT * FROM `games` WHERE `cost` > ?");
-              break;
+        if ($search) {
+          if ($select == "cost") {
+            switch ($_POST['costSort']) {
+              case "<":
+                $query = $pdo->prepare("SELECT * FROM `games` WHERE `cost` < ?");
+                break;
+              case "<=":
+                $query = $pdo->prepare("SELECT * FROM `games` WHERE `cost` <= ?");
+                break;
+              case "=":
+                $query = $pdo->prepare("SELECT * FROM `games` WHERE `cost` = ?");
+                break;
+              case ">=":
+                $query = $pdo->prepare("SELECT * FROM `games` WHERE `cost` >= ?");
+                break;
+              case ">":
+                $query = $pdo->prepare("SELECT * FROM `games` WHERE `cost` > ?");
+                break;
+            }
+          } else if ($select == "rating") {
+            $query = $pdo->prepare("SELECT * FROM `games` WHERE `rating` = ?");
+          } else if ($select == "name") {
+            $query = $pdo->prepare("SELECT * FROM `games` WHERE `name` LIKE ?");
+            $search = "%$search%";
           }
-        } else if ($select == "rating") {
-          $query = $pdo->prepare("SELECT * FROM `games` WHERE `rating` = ?");
-        } else if ($select == "name") {
-          $query = $pdo->prepare("SELECT * FROM `games` WHERE `name` LIKE ?");
-          $search = "%$search%";
+          $query->execute([$search]);
+          // Loop through the set of results, one record at a time
+          foreach ($query as $record) {
+            print "<tr>
+              <td>" . "<p>" . $record['name'] . "</p><br>" . "</td>
+              <td>" . "<p>" . $record['cost'] . "</p><br>" . "</td>
+              <td>" . "<p>" . $record['rating'] . "</p><br>" . "</td>
+              <td>" . "<p>" . $record['instock'] . "</p><br>" . "</td>
+              <td>" . "<div class=\"picture\">
+                <a href=\"/view.php?id=" . $record['id'] . "\" target=\"_blank\">
+                  <img src=\"/" . $record['images'] . "\" alt=\"" . $record['name'] . "\" />
+                </a>
+              </div><br>" . "</td>
+            </tr>";
+          }
         }
-        $query->execute([$search]);
-        // Loop through the set of results, one record at a time
-        foreach ($query as $record) {
-          print "<tr>
-            <td>" . "<p>" . $record['name'] . "</p><br>" . "</td>
-            <td>" . "<p>" . $record['cost'] . "</p><br>" . "</td>
-            <td>" . "<p>" . $record['rating'] . "</p><br>" . "</td>
-            <td>" . "<p>" . $record['instock'] . "</p><br>" . "</td>
-            <td>" . "<div class=\"picture\">
-              <a href=\"/view.php?id=" . $record['id'] . "\" target=\"_blank\">
-                <img src=\"/" . $record['images'] . "\" alt=\"" . $record['name'] . "\" />
-              </a>
-            </div><br>" . "</td>
-          </tr>";
-        }
-      }
       $query = null;
       $pdo = null;
+    }
       ?>
 
     </table>
